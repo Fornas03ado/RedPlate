4244d084377c09dff0108637820216e0f9e4cb7e
fornas <fornas03professional@gmail.com>
2022-05-05 16:54:02 +0200

    First commit

---
+++ app/src/main/java/com/aasoftware/redplate/domain/DomainDataClasses.kt
@@ -0,0 +1,6 @@
+package com.aasoftware.redplate.domain
+
+import android.location.Location
+
+data class Gym(var id: Int, var name: String, var location: Location)
+data class User(val name: String)
